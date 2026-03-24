cef66a25f996786427ae819a583ad3da4795d2e4
Rafael <rafaelnascimentoo.dev@gmail.com>
2026-03-24 15:18:15 -0300

    feat(user): implement CustomUserDetailsService

---
+++ src/main/java/com/nutrition_system/repository/UsersRepository.java
@@ -0,0 +1,11 @@
+package com.nutrition_system.repository;
+
+import com.nutrition_system.entity.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UsersRepository extends JpaRepository<Users, UUID> {
+    Optional<Users> findByUsername(String username);
+}
---
+++ src/main/java/com/nutrition_system/service/CustomUserDetailsService.java
@@ -0,0 +1,30 @@
+package com.nutrition_system.service;
+
+import com.nutrition_system.entity.Users;
+import com.nutrition_system.repository.UsersRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UsersRepository usersRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+
+        Users user = usersRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+
+
+        return org.springframework.security.core.userdetails.User
+                .builder()
+                .username(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRole())
+                .build();
+    }
+}
