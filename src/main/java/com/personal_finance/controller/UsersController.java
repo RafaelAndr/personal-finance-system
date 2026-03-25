6b11db5193e5ce9d8cf404ee6b4527a252ada03b
Rafael <rafaelnascimentoo.dev@gmail.com>
2026-03-25 15:12:13 -0300

    feat(user): implement registration

--- src/main/java/com/personal_finance/controller/UsersController.java
+++ src/main/java/com/personal_finance/controller/UsersController.java
@@ -1,9 +1,25 @@
 package com.personal_finance.controller;
 
+import com.personal_finance.entity.Users;
+import com.personal_finance.service.UsersService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
+@RequiredArgsConstructor
 public class UsersController {
+
+    private final UsersService usersService;
+
+    @PostMapping
+    public ResponseEntity<Users> create(@RequestBody Users user){
+        Users userCreated = usersService.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
+    }
 }
--- src/main/java/com/personal_finance/service/UsersService.java
+++ src/main/java/com/personal_finance/service/UsersService.java
@@ -1,5 +1,6 @@
 package com.personal_finance.service;
 
+import com.personal_finance.entity.Users;
 import com.personal_finance.repository.UsersRepository;
 import lombok.RequiredArgsConstructor;
 
@@ -7,4 +8,8 @@ import lombok.RequiredArgsConstructor;
 public class UsersService {
 
     private final UsersRepository usersRepository;
+
+    public Users save(Users user) {
+        return usersRepository.save(user);
+    }
 }
