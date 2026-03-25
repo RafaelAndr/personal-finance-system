cf930848ff4c46c7d6ce5431f6da28c4c79fe310
Rafael <rafaelnascimentoo.dev@gmail.com>
2026-03-25 15:20:07 -0300

    fix(user): fix bug of bean required

--- src/main/java/com/personal_finance/service/UsersService.java
+++ src/main/java/com/personal_finance/service/UsersService.java
@@ -3,12 +3,16 @@ package com.personal_finance.service;
 import com.personal_finance.entity.Users;
 import com.personal_finance.repository.UsersRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
+@Service
 public class UsersService {
 
     private final UsersRepository usersRepository;
 
+    @Transactional
     public Users save(Users user) {
         return usersRepository.save(user);
     }
