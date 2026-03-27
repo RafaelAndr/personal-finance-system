277c9eb177783e65951bef3336cb9346454f40b2
Rafael <rafaelnascimentoo.dev@gmail.com>
2026-03-26 21:33:18 -0300

    feat(security): create JwtUserDetails

---
+++ src/main/java/com/personal_finance/security/JwtUserDetails.java
@@ -0,0 +1,29 @@
+package com.personal_finance.security;
+
+import com.personal_finance.entity.Users;
+import com.personal_finance.entity.enums.Role;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+import java.util.UUID;
+
+public class JwtUserDetails extends User {
+
+    private Users user;
+
+    public JwtUserDetails(Users user) {
+        super(user.getUsername(), user.getPassword(), AuthorityUtils.createAuthorityList(user.getRole().name()));
+        this.user = user;
+    }
+
+    public UUID getId() {
+        return this.user.getId();
+    }
+
+    public Role getRole() {
+        return this.user.getRole();
+    }
+}
