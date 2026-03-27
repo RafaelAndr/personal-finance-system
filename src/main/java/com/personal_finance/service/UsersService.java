34debb46ec0b275ba01c6da3b80d40be4249b28f
Rafael <rafaelnascimentoo.dev@gmail.com>
2026-03-26 21:35:06 -0300

    feat(user): create user service

--- src/main/java/com/personal_finance/service/UsersService.java
+++ src/main/java/com/personal_finance/service/UsersService.java
@@ -3,9 +3,12 @@ package com.personal_finance.service;
 import com.personal_finance.dto.response.UserResponseDto;
 import com.personal_finance.dto.resquest.UserRequestDto;
 import com.personal_finance.entity.Users;
+import com.personal_finance.entity.enums.Role;
 import com.personal_finance.mapper.UserMapper;
 import com.personal_finance.repository.UsersRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,9 +21,11 @@ public class UsersService {
 
     private final UsersRepository usersRepository;
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional //só precisa usar transaction quando há mais de uma operação no metodo
     public Users save(Users user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return usersRepository.save(user);
     }
 
@@ -41,5 +46,17 @@ public class UsersService {
     @Transactional(readOnly = true)
     public List<Users> findAll() {
         return usersRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public Users searchByUsername(String username) {
+        return usersRepository.findByUsername(username).orElseThrow(
+                () -> new EntityNotFoundException(String.format("Username '%s' not found", username))
+        );
+    }
+
+    @Transactional(readOnly = true)
+    public Role searchRoleByUsername(String username) {
+        return usersRepository.findRoleByUsername(username);
     }
 }
