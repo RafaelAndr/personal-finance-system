c376f01ae5de59be4661cf054b1953b4ee3f2a70
Rafael <rafaelnascimentoo.dev@gmail.com>
2026-04-08 12:09:21 -0300

    feat(income): add search methods in repository

--- src/main/java/com/personal_finance/repository/IncomeRepository.java
+++ src/main/java/com/personal_finance/repository/IncomeRepository.java
@@ -1,9 +1,15 @@
 package com.personal_finance.repository;
 
+import com.personal_finance.entity.Account;
 import com.personal_finance.entity.Income;
+import com.personal_finance.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface IncomeRepository extends JpaRepository<Income, UUID> {
+
+    List<Income> findByUser(Users user);
+    List<Income> findByAccount(Account account);
 }
